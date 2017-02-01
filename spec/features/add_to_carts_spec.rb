e6372bc58722a024eea4242a90bf8c1453503d9e
Dom Golac <domgolac@gmail.com>
2017-02-01 21:45:22 +0000

    Completed feature/testing-add-to-cart

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor adds product to cart", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99,
+      )
+    end
+  end
+
+  scenario do
+    visit root_path
+    first('article.product footer.actions a.btn.btn-primary').click
+    sleep 1
+    save_screenshot
+    expect(page.find('div#navbar ul.nav.navbar-nav.navbar-right')).to have_text 'My Cart (1)'
+  end
+  
+end
